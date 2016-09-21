fdce00e97777982cabf5d6307fa08da309d97353
liyan <lidoyan@gmail.com>
2016-09-21 17:42:02 +0800

    change eventhandler function binding.
    add the eventhandler function by closure to make the button target the
    right feed.

--- js/zhihuGender.js
+++ js/zhihuGender.js
@@ -1,3 +1,5 @@
+// Add a button for every zhihu-feed to scroll to the next feed.
+
 var feeds = document.getElementsByClassName('feed-item')
 function getElementTop(element){
 　　　　var actualTop = element.offsetTop;
@@ -9,16 +11,22 @@ function getElementTop(element){
 　　　　return actualTop;
 }
 function scrollToElement(element){
+    // a builtin function can get this without calculate the offsetTop: element.scrollIntoView()
     var eleTopPos = getElementTop(element)
     window.scrollTo(0, eleTopPos)
 }
-for (var i=0, len=feeds.length; i<len; i++){
-    var nextButton = document.createElement("button")
-    nextButton.style.cssText = "position:fixed; left:581px; bottom:100px; height:30px; width:50px; color:blue"
-    nextButton.innerHTML = "next"
+function makeItWork(){
     var nextFeed = feeds[i+1]
     nextButton.onclick = function(){
         scrollToElement(nextFeed)
     }
+}
+for (var i=0, len=feeds.length; i<len; i++){
+    var nextButton = document.createElement("button")
+    nextButton.style.cssText = "position:absolute; left:581px; bottom:100px; height:30px; width:50px;"
+    nextButton.innerHTML = "next"
+    makeItWork()
+    // The previous one wont't work cause it's eventhandler function bound to "NextFeed" which will always be the next of last feed
+    // to nextFeed exists, nextFeed target the right feed is bound to the right button.
     feeds[i].appendChild(nextButton)
 }
