9e6ad3c916671d2e4f57e7280cb7fc24f30b7aa9
liyan <lidoyan@gmail.com>
2016-09-23 11:44:07 +0800

    how Class.x and obj.x behave.
    if no __get__ implemented in descriptor x,
    Class.x will get descriptor instance.
    obj.x will refer to Class.x.
    
    otherwise Class.x will call x's __get__ , same as obj.x.(if overrided,
    if not, obj.x will get instance attribute x instead call x.__get__)

--- python/getattr.py
+++ python/getattr.py
@@ -149,7 +149,7 @@ class Item4:
 
         'self.weight = weight' equals to 'Item4.weight.__set__(self, weight)'
         first weight is the descriptor instance, second weight is the argument weight.
-        ***No behave this if weight implements __get__ for that Item4.weight call weight's __get__ instead of get Quanlity object..
+        ***It won't behave like this if weight implements __get__ for that Item4.weight will call weight's __get__ instead of get Quanlity object.
 
         """
         self.weight = weight    # inside it: Item4.weight.__set__(self, weight) just cause no __get__ implemented in Quantity('weight').
