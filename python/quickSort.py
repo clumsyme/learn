f04e949db6d3d9ea3cab17edeb04770987363fd8
liyan <lidoyan@gmail.com>
2016-10-05 19:26:09 +0800

    explain a common error

--- python/quickSort.py
+++ python/quickSort.py
@@ -1,4 +1,18 @@
-def Qsort(table, left=0, right=None):
+"""
+Note that codes from line 30 through line 39 can't be written in this way:
+
+i = left + 1
+j = right - 2
+while True:
+    while table[i] < pivot:
+        i += 1
+    while table[j] > pivot:
+        j -= 1
+
+It may cause an infinite loop if table[i] == table[j] == pivot.
+"""
+
+def quicksort(table, left=0, right=None):
     if right == None:
         right = len(table) - 1
     center = (left+right) //2
@@ -8,7 +22,7 @@ def Qsort(table, left=0, right=None):
         table[center], table[right] = table[right], table[center]
     if table[left] > table[center]:
         table[left], table[center] = table[center], table[left]
-    if right-left <= 2:
+    if right-left <= 2:     # If table size <= 3, it's sorted by now.
         return
     table[center], table[right-1] = table[right-1], table[center]
     pivot = table[right-1]
@@ -28,5 +42,5 @@ def Qsort(table, left=0, right=None):
         else:
             break
     table[i], table[right-1] = table[right-1], table[i]
-    Qsort(table, left, i-1)
-    Qsort(table, i+1, right)
+    quicksort(table, left, i-1)
+    quicksort(table, i+1, right)
