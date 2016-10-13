778886eababb7a57f64c9d201ef0f303c02096f2
liyan <lidoyan@gmail.com>
2016-10-13 09:22:29 +0800

    now it can really work as a web server

--- python/webServer.py
+++ python/webServer.py
@@ -8,15 +8,19 @@ serverSocket = socket.socket()
 serverSocket.bind(('localhost', 8888))
 
 serverSocket.listen(5)
-clientSocket, addr = serverSocket.accept()
-print('connected from ', addr)
-request = clientSocket.recv(1024)
-src = re.findall(pattern, request.decode())[0]+'.html'
-if not os.path.isfile(src):
-    with open('404.html', 'rb') as fzf:
-        response = fzf.read()
-else:
-    with open(src, 'rb') as file:
-        response = file.read()
-clientSocket.send(response)
-clientSocket.close()
+while True:
+    clientSocket, addr = serverSocket.accept()
+    print('connected from ', addr)
+    request = clientSocket.recv(1024)
+    src = re.findall(pattern, request.decode())
+    if src:
+        src = src[0]
+    print('request', src)
+    if not os.path.isfile(src):
+        with open('404.html', 'rb') as fzf:
+            response = fzf.read()
+    else:
+        with open(src, 'rb') as file:
+            response = file.read()
+    clientSocket.send(response)
+    clientSocket.close()
