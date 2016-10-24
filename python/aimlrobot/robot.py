2c272bb051a01e50fd604c38f3afe51fb02bfbbb
liyan <lidoyan@gmail.com>
2016-10-24 19:29:45 +0800

    save learned to a brain file

--- python/aimlrobot/robot.py
+++ python/aimlrobot/robot.py
@@ -1,8 +1,13 @@
 import aiml
+import os
+import sys
 
 kernel = aiml.Kernel()
-kernel.learn('std-startup.xml')
-kernel.respond('load aiml b')
+if len(sys.argv) == 1:
+    kernel.bootstrap(brainFile='brain.brn')
+else:
+    kernel.bootstrap(learnFiles='std-startup.xml', commands='load aiml basic')
+    kernel.saveBrain('brain.brn')
 
 while True:
     print(kernel.respond(raw_input('Enter:')))
