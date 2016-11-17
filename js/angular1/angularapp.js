2d03db811e938bd25912dd38c7dd1fe957fbe336
liyan <lidoyan@gmail.com>
2016-11-17 23:07:19 +0800

    angular1 examples

---
+++ js/angular1/angularapp.js
@@ -0,0 +1,60 @@
+function fbn(n) {
+    var nums = [];
+    var a = b = 1;
+    for (let i = 0; i < n; i++) {
+        [a, b] = [b, a + b];
+        nums.push(a);
+    }
+    return nums
+}
+
+var app = angular.module('app', [])
+app.controller('capp', function ($scope, $location, $timeout, $interval, $http) {
+    $scope.price = 0
+    $scope.count = 0
+    $scope.fn = 3
+    $scope.rootUrl = $location.absUrl()
+    $scope.countdown = 5
+    $scope.seeindex = true
+    $scope.fruits = ['apple', 'banana', 'orange']
+    $scope.disableit = function() {
+        $scope.cantuse = true
+        $scope.showenable = true
+    }
+    $scope.enableit = function() {
+        $scope.cantuse = false
+        $scope.showenable = false
+    }
+    $scope.players = [
+        {
+            name: 'Kobe',
+            position: 'Guard',
+            score: 81
+        },
+        {
+            name: 'Pau',
+            position: 'Center',
+            score: 30
+        },
+        {
+            name: 'Lamar',
+            position: 'Forward',
+            score: 28
+        }
+    ]
+    $interval(function() {
+        $scope.countdown -= 1
+    }, 1000)
+    $timeout(function() {
+        $scope.message = 'Nice to meet you!'
+    }, 5000)
+    $http.get('movies.json').success(
+        function(response){
+            $scope.movies = response.movies
+        }
+    )
+    $scope.nums = function() {
+        return fbn($scope.fn)
+    }
+})
+
