1c0e0c509bd74f043edcb15a0ed94b3a60c34128
liyan <lidoyan@gmail.com>
2016-12-29 12:13:00 +0800

    angular

--- js/angular1/angularapp.js
+++ js/angular1/angularapp.js
@@ -70,5 +70,30 @@ myapp.controller('capp', function ($scope, $location, $timeout, $interval, $http
     $scope.nums = function() {
         return fbn($scope.fn)
     }
+    $scope.changebk = function(color) {
+        console.log('okk')
+        document.getElementsByTagName('body')[0].style.color = color
+    }
+    $scope.ctemp = ""
+    $scope.ftemp = ""
+    $scope.ctempchange = function() {
+        var ftemp = 9 * $scope.ctemp / 5 + 32
+        if (typeof ftemp === "number" && $scope.ctemp !== "" ) {
+            $scope.ftemp = ftemp
+        }else {
+            $scope.ftemp = ""
+        }
+    }
+    $scope.ftempchange = function() {
+        var ctemp = 5 * ($scope.ftemp - 32) / 9
+        if (typeof ctemp === "number" && $scope.ftemp !== "" ) {
+            $scope.ctemp = ctemp
+        }else {
+            $scope.ctemp = ""
+        }
+    }
+    $scope.boil = function() {
+        return ($scope.ctemp >= 100)
+    }
 })
 
