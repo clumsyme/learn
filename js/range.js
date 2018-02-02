6a2914dd346fc3908a47e1632b2ea468900b09ab
LiYan <lidoyan@gmail.com>
2018-02-02 11:30:13 +0800

    usage

--- js/range.js
+++ js/range.js
@@ -24,4 +24,27 @@ function range(low, high, step) {
     return new Range(low, high, step)
 }
 
+`
+// usage
+Array.from(range(5,19,6))
+// [5, 11, 17]
+range(10)
+// Range {low: 0, high: 10, step: 1}
+range(6, 12)
+// Range {low: 6, high: 12, step: 1}
+range(6, 12, 3)
+// Range {low: 6, high: 12, step: 3}
 
+for (let i of range(3)) {
+    console.log(i)
+}
+// 0
+// 1
+// 2
+
+[...range(3, 10)]
+// [3, 4, 5, 6, 7, 8, 9]
+
+[...range(6, 33, 5)]
+// [6, 11, 16, 21, 26]
+`
